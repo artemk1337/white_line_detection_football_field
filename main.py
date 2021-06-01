b92a6d7479db7c586fe11a87454c67b766547c6f
Artem K <artemkush1@gmail.com>
2021-06-01 03:33:57 +0300

    fix

--- main.py
+++ main.py
@@ -1,109 +1,95 @@
-from PIL import Image
+import cv2 as cv
 import numpy as np
-from scipy.ndimage import gaussian_filter
-from skimage.transform import probabilistic_hough_line
-import matplotlib.pyplot as plt
-import copy
-import cv2
 
 
-img = cv2.imread("2.jpg")
+def filter(img):
+    img = cv.resize(img, (1920, 1080))
+    kernel = np.ones((15, 15), np.uint8)
 
-print(img.shape)
+    hsv = cv.cvtColor(cv.GaussianBlur(img, (5, 5), 5), cv.COLOR_BGR2HSV)
+    mask_green = cv.inRange(hsv, (20, 20, 90), (70, 235, 200))
 
-square_size = 10
+    mask_green = cv.morphologyEx(mask_green, cv.MORPH_OPEN, kernel)
 
-
-# x1, x2, y1, y2 = 0, 75, 300, 375
-# print(img[y1:y2, x1:x2].mean(axis=(0, 1)))
-# cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 3)
-# cv2.imshow("Test", img)
-# cv2.waitKey()
-# exit()
-
-
-# print(img[np.where(img < 100)])
-# img[np.where(img < 100)] = 0
-# cv2.imshow("Test", img)
-# cv2.waitKey()
-
-res = np.zeros_like(img)
-
-for x_ in range(0, img.shape[1] - square_size, 10):
-    for y_ in range(0, img.shape[0] - square_size, 10):
-        mean_val = img[y_:y_+square_size, x_:x_+square_size].mean(axis=(0, 1))
-        if mean_val[1] > 80 and np.argmax(mean_val) == 1:
-            res[y_:y_ + square_size, x_:x_ + square_size] = img[y_:y_+square_size, x_:x_+square_size]
-    print(x_, end="\r")
-
-img = res
-
-cv2.imshow("Test", res)
-cv2.waitKey()
+    components = cv.connectedComponentsWithStats(mask_green, 4, cv.CV_32S)
+    max_area_index = (np.argmax(components[2], axis=0))[4]
+    for i in range(len(components[1])):
+        for j in range(len(components[1][i])):
+            if (components[1][i][j] != max_area_index):
+                mask_green[i][j] = 0
+    mask_green = cv.morphologyEx(mask_green, cv.MORPH_CLOSE, kernel)
+    img = cv.bitwise_and(img, img, mask=mask_green)
+    return img
 
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("Test", gray)
-# cv2.waitKey()
+original = cv.imread('2.jpg')
 
+img = filter(original)
+cv.imshow('Test1', img)
+cv.waitKey()
 
-(thresh, gray) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-# cv2.imshow("Test", gray)
-# cv2.waitKey()
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow("Test", gray)
+# cv.waitKey()
 
+(thresh, gray) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+# cv.imshow("Test", gray)
+# cv.waitKey()
 
-kernel_size = 15
-gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
-# cv2.imshow("Test", gray)
-# cv2.waitKey()
+# kernel_size = 15
+# gray = cv.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+# cv.imshow("Test", gray)
+# cv.waitKey()
 
+edges = cv.Canny(gray, 50, 150)
+cv.imshow("Test", edges)
+cv.waitKey()
 
-edges = cv2.Canny(gray, 50, 150)
-# cv2.imshow("Test", edges)
-# cv2.waitKey()
-
-
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=75, maxLineGap=10)
+lines = cv.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=75, maxLineGap=20)
 print(lines.shape)
 
-
-img = np.zeros_like(img)
+img = np.zeros((img.shape[0], img.shape[1]))
 for line in lines:
     x1, x2, y1, y2 = line[0]
-    cv2.line(img, (x1, x2), (y1, y2), (0, 0, 255), 5)
+    cv.line(img, (x1, x2), (y1, y2), 255, 15)
 
-cv2.imshow("Test", img)
-cv2.waitKey()
-cv2.imwrite("RES.png", img)
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (21, 21))
+img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel).astype(np.uint8)
+cv.imshow("Test", img)
+cv.waitKey()
 
 
-# lines = cv2.HoughLines(edges, 1.5, np.pi/180, 50)
-# for rho, theta in lines[0]:
-#     a = np.cos(theta)
-#     b = np.sin(theta)
-#     x0 = a*rho
-#     y0 = b*rho
-#     x1 = int(x0 + 1000*(-b))
-#     y1 = int(y0 + 1000*(a))
-#     x2 = int(x0 - 1000*(-b))
-#     y2 = int(y0 - 1000*(a))
+
+
+
+
+############
+
+
+#################################   SIFT   ####################################
+# sift = cv.SIFT_create()
 #
-#     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+# img_kp, img_ds = sift.detectAndCompute(grayscale, None)
+# field_kp, field_ds = sift.detectAndCompute(field, None)
 #
-# cv2.imshow("Test", img)
-# cv2.waitKey()
-
-# lines = probabilistic_hough_line(edges, threshold=10, line_length=100,
-#                                  line_gap=10)
-# plt.imshow(edges, cmap='gray')
-# for line in lines:
-#     p0, p1 = line
-#     plt.plot((p0[0], p1[0]), (p0[1], p1[1]))
-# plt.show()
-
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-# for i in range(3):
-#     cv2.imshow("Test", img[:, :, i])
-#     cv2.waitKey()
-
-
+# cv.drawKeypoints(original, img_kp, original, color=(255, 0, 0))
+#
+# cv.imshow('qwe', original)
+# cv.waitKey()
+#
+# matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
+#
+# local_matches = matcher.knnMatch(field_ds, img_ds, k=2)
+# good = []
+# for m, mm in local_matches:
+#     if m.distance < 0.75 * mm.distance:
+#         good.append(m)
+# matches = good
+#
+# src_pts = np.float32([img_kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+# dst_pts = np.float32([field_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+# matrix = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+# dst = cv.warpPerspective(src=original, M=matrix[0], dsize=(1200, 800))[0]
+#
+# cv.imshow('dst.jpg', dst)
+# cv.waitKey()
