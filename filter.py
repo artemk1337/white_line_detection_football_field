dc53a4d48f8258b534dee8e173f1106501bac2dc
Artem K <artemkush1@gmail.com>
2021-06-01 23:16:57 +0300

    fix

---
+++ filter.py
@@ -0,0 +1,154 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from PIL import ImageEnhance, Image
+
+
+def plot_image(img, cmap=None):
+    plt.axis("off")
+    plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB), cmap=cmap)
+    plt.show()
+
+
+class ImageHolder:
+    def __init__(self, filename: str):
+        self.src_img = cv.imread(filename)
+        self.src_img = cv.resize(self.src_img, (1920, 1080))
+        self.dst_img = None
+
+    def load_img(self, filename: str):
+        self.src_img = cv.imread(filename)
+        self.src_img = cv.resize(self.src_img, (1920, 1080))
+        self.dst_img = None
+
+    def _filterMark_(self, img):
+        # img = cv.resize(img, (1920, 1080))
+        kernel = np.ones((15, 15), np.uint8)
+
+        hsv = cv.cvtColor(cv.GaussianBlur(img, (5, 5), 5), cv.COLOR_BGR2HSV)
+        mask_green = cv.inRange(hsv, (20, 20, 60), (70, 235, 200))
+
+        mask_green = cv.morphologyEx(mask_green, cv.MORPH_OPEN, kernel)
+
+        components = cv.connectedComponentsWithStats(mask_green, 4, cv.CV_32S)
+        max_area_index = (np.argmax(components[2], axis=0))[4]
+        for i in range(len(components[1])):
+            for j in range(len(components[1][i])):
+                if (components[1][i][j] != max_area_index):
+                    mask_green[i][j] = 0
+        mask_green = cv.morphologyEx(mask_green, cv.MORPH_CLOSE, kernel)
+        img = cv.bitwise_and(img, img, mask=mask_green)
+        return img
+
+    def _a_b_x_y_list_(self, lines):
+        a_b_x_y = []
+        a_b = []
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            if not (x2 - x1):
+                break
+            if (x2 < x1):
+                print(x1, x2)
+            a = (y2 - y1) / (x2 - x1)
+            if abs(a) < 5:
+                b = y1 - a * x1
+                a = np.arctan(a) * 180
+                a_b += [[a, b]]
+                a_b_x_y += [[a, b, line[0]]]
+        return a_b_x_y, a_b
+
+    def _create_clusters_(self, a_b):
+        clustering = DBSCAN(eps=30, min_samples=1)
+        clusters = clustering.fit_predict(a_b)
+        clustersU = np.unique(clusters)
+        return clusters, clustersU
+
+    def _create_cluster_dict_(self, a_b_x_y, clusters):
+        clusters_dict = {}
+        for cluster in clusters: clusters_dict[cluster] = []
+        for i, cluster in enumerate(clusters):
+            a, b, [x1, y1, x2, y2] = a_b_x_y[i]
+            clusters_dict[cluster] += [[a, b, [x1, y1, x2, y2]]]
+        return clusters_dict
+
+    def _find_best_points_(self, clusters_dict, clustersU, plot=True):
+        total_points = []
+
+        for cluster in clustersU:
+            zeros = np.zeros((self.src_img.shape[0], self.src_img.shape[1])).astype(np.uint8)
+
+            pair1, pair2 = [], []
+            a_b_mean = []
+
+            for line in clusters_dict[cluster]:
+                a, b, [x1, y1, x2, y2] = line
+                pair1 += [[x1, y1]]
+                pair2 += [[x2, y2]]
+                a_b_mean += [[a, b]]
+                cv.line(zeros, (x1, y1), (x2, y2), 255, 2)
+
+            #             print(a, b, x1, y1, x2, y2)
+            #             plt.plot(a, b, ".")
+            #         plt.show()
+
+            #         plot_image(zeros)
+            #         return -1
+
+            a_b_mean = np.mean(a_b_mean, axis=0)
+            # a_b_mean = a_b_mean[0]
+
+            min_dist, best_points = 0, []
+            for x1, y1 in pair1:
+                for x2, y2 in pair2:
+                    dist = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                    if dist > min_dist:
+                        min_dist = dist
+                        best_points = [x1, y1, x2, y2]
+
+            x1, y1, x2, y2 = best_points
+            if plot:
+                cv.line(zeros, (x1, y1), (x2, y2), 255, 2)
+                plot_image(zeros)
+            a, b = a_b_mean
+
+            total_points += [[a, b, best_points]]
+        return total_points
+
+    def apply_filters(self):
+        img = self.src_img
+        img = self._filterMark_(img)
+
+        morph = cv.morphologyEx(img, cv.MORPH_TOPHAT, np.ones((15, 15), np.uint8))
+        gray = morph[:, :, np.argmax(morph.mean(axis=(0, 1)))]
+
+        kernel_size = 5
+        gauss = cv.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+        edges = cv.Canny(gauss, 50, 150)
+
+        lines = cv.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=180, maxLineGap=50)
+
+        # zeros = np.zeros((img.shape[0], img.shape[1])).astype(np.uint8)
+        # kernal_size = 15
+        # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (kernal_size, kernal_size))
+        # morph2 = cv.morphologyEx(zeros, cv.MORPH_CLOSE, kernel).astype(np.uint8)
+
+        a_b_x_y, a_b = self._a_b_x_y_list_(lines)
+
+        clusters, clustersU = self._create_clusters_(a_b)
+
+        clusters_dict = self._create_cluster_dict_(a_b_x_y, clusters)
+
+        total_points = self._find_best_points_(clusters_dict, clustersU, False)
+
+        self.dst_img = self.src_img.copy()
+        # self.dst_img = np.zeros((img.shape[0], img.shape[1])).astype(np.uint8)
+        for a, b, [x1, y1, x2, y2] in total_points:
+            cv.line(self.dst_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        return total_points, self.dst_img
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,95 +1,16 @@
 import cv2 as cv
 import numpy as np
+from filter import ImageHolder
+import multiprocessing
 
 
-def filter(img):
-    img = cv.resize(img, (1920, 1080))
-    kernel = np.ones((15, 15), np.uint8)
-
-    hsv = cv.cvtColor(cv.GaussianBlur(img, (5, 5), 5), cv.COLOR_BGR2HSV)
-    mask_green = cv.inRange(hsv, (20, 20, 90), (70, 235, 200))
-
-    mask_green = cv.morphologyEx(mask_green, cv.MORPH_OPEN, kernel)
-
-    components = cv.connectedComponentsWithStats(mask_green, 4, cv.CV_32S)
-    max_area_index = (np.argmax(components[2], axis=0))[4]
-    for i in range(len(components[1])):
-        for j in range(len(components[1][i])):
-            if (components[1][i][j] != max_area_index):
-                mask_green[i][j] = 0
-    mask_green = cv.morphologyEx(mask_green, cv.MORPH_CLOSE, kernel)
-    img = cv.bitwise_and(img, img, mask=mask_green)
-    return img
 
 
-original = cv.imread('2.jpg')
+# multiprocessing.Process()
 
-img = filter(original)
-cv.imshow('Test1', img)
+
+imh = ImageHolder("images/3.jpg")
+_, im = imh.apply_filters()
+cv.imshow("test", im)
 cv.waitKey()
 
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-# cv.imshow("Test", gray)
-# cv.waitKey()
-
-(thresh, gray) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
-# cv.imshow("Test", gray)
-# cv.waitKey()
-
-# kernel_size = 15
-# gray = cv.GaussianBlur(gray, (kernel_size, kernel_size), 0)
-# cv.imshow("Test", gray)
-# cv.waitKey()
-
-edges = cv.Canny(gray, 50, 150)
-cv.imshow("Test", edges)
-cv.waitKey()
-
-lines = cv.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=50, minLineLength=75, maxLineGap=20)
-print(lines.shape)
-
-img = np.zeros((img.shape[0], img.shape[1]))
-for line in lines:
-    x1, x2, y1, y2 = line[0]
-    cv.line(img, (x1, x2), (y1, y2), 255, 15)
-
-kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (21, 21))
-img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel).astype(np.uint8)
-cv.imshow("Test", img)
-cv.waitKey()
-
-
-
-
-
-
-############
-
-
-#################################   SIFT   ####################################
-# sift = cv.SIFT_create()
-#
-# img_kp, img_ds = sift.detectAndCompute(grayscale, None)
-# field_kp, field_ds = sift.detectAndCompute(field, None)
-#
-# cv.drawKeypoints(original, img_kp, original, color=(255, 0, 0))
-#
-# cv.imshow('qwe', original)
-# cv.waitKey()
-#
-# matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
-#
-# local_matches = matcher.knnMatch(field_ds, img_ds, k=2)
-# good = []
-# for m, mm in local_matches:
-#     if m.distance < 0.75 * mm.distance:
-#         good.append(m)
-# matches = good
-#
-# src_pts = np.float32([img_kp[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-# dst_pts = np.float32([field_kp[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-# matrix = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-# dst = cv.warpPerspective(src=original, M=matrix[0], dsize=(1200, 800))[0]
-#
-# cv.imshow('dst.jpg', dst)
-# cv.waitKey()
